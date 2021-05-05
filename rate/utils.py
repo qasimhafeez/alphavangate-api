e2f1bc2b9aa5cd07419b3282420767df2874bc3a
Qasim Hafeez <qasimh556@gmail.com>
2021-05-05 23:55:41 +0400

    get_rate_from_db

--- rate/utils.py
+++ rate/utils.py
@@ -1,10 +1,14 @@
-import requests
 import json
+import os
+
+import requests
 from rest_framework.status import HTTP_200_OK
+from .models import BtcRate
 
 
 def get_rate():
-    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=RPECBEU9BGLGO8AN'
+    api_key = os.getenv('API_KEY')
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey={api_key}'
     response = requests.get(url)
     if response.status_code == HTTP_200_OK:
         result = json.loads(response.text)['Realtime Currency Exchange Rate']['5. Exchange Rate']
@@ -12,3 +16,7 @@ def get_rate():
         result = 'Rate not available'
 
     return result
+
+
+def get_rate_from_db():
+    return BtcRate.objects.last().exchange_rate
