a62e8aa408224ec6ca31991ea062192bd68ff4a4
Qasim Hafeez <qasimh556@gmail.com>
2021-05-05 23:54:26 +0400

    postgres config

--- alphavantage/settings.py
+++ alphavantage/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'alphavantage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'alphavantage'),
+        'USER': os.getenv('DB_USER', 'qasimhafeez'),
+        'PASSWORD': os.getenv('DB_PASS', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 
