4a545a7ef63a3a478e9e2d4f346003805090265a
Qasim Hafeez <qasimh556@gmail.com>
2021-05-05 14:26:42 +0400

    views

--- rate/views.py
+++ rate/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .utils import get_rate
 
-# Create your views here.
+
+class GetRate(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    def get(self, request):
+        a = Response({'Exchange_rate': get_rate()})
+        return a
+
+    def post(self, request):
+        pass
